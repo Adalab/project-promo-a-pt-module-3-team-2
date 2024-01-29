e5e673fdee1af8ef387e33c220b25c7e0f482e76
maialenmunoa <maialenmunoa@gmail.com>
2024-01-29 16:09:56 +0100

    fixed logo and laptop in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,16 @@
 import '../scss/Header.scss';
+import laptopCodeLogo from '../images/laptop-code-solid.svg';
+import adalabLogo from '../images/adalab.png';
 
 function Header() {
     return (
-        <header className="header">
-        <a className="header__brand" href="./" title="Haz click para volver a la página inicial">
-          <img className="header__companyLogo" src="./images/laptop-code-solid.svg" alt="Logo proyectos molones" />
-          <h1 className="header__title">Proyectos molones</h1>
-        </a>
-        <img className="logoSponsor" src="./images/adalab.png" alt="Logo Adalab" />
-        </header>
+      <header className="header">
+      <a className="header__brand" href="./" title="Haz click para volver a la página inicial">
+        <img className="header__companyLogo" src={laptopCodeLogo} alt="Logo proyectos molones" />
+        <h1 className="header__title">Proyectos molones</h1>
+      </a>
+      <img className="logoSponsor" src={adalabLogo} alt="Logo Adalab" />
+      </header>
     )
 }
 
