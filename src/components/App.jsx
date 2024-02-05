95819651be107caf2d5880749b32266526e44ea4
iewaw <izabelawawrzyniak90@gmail.com>
2024-02-05 09:08:24 +0100

    adding data const

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,34 @@
-import '../scss/App.scss';
-import Header from './Header';
-import Hero from './Hero';
-import Card from './Card';
-import Form from './Form';
-import Footer from './Footer';
+import "../scss/App.scss";
+import Header from "./Header";
+import Hero from "./Hero";
+import Card from "./Card";
+import Form from "./Form";
+import Footer from "./Footer";
+import { useState } from "react";
 
 function App() {
+  const [data, setData] = useState({
+    name: "",
+    slogan: "",
+    technologies: "",
+    repo: "",
+    demo: "",
+    desc: "",
+    author: "",
+    job: "",
+    photo: "",
+    image: "",
+  });
+
   return (
     <div>
-    <Header />
+      <Header />
 
-
-    <main className="main">
-      <Hero />
-      <Card />
-      <Form />
-
-    </main>
+      <main className="main">
+        <Hero />
+        <Card />
+        <Form />
+      </main>
       <Footer />
     </div>
   );
