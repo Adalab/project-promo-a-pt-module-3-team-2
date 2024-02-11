3e7bf8b4ffee2e2f8ed4a9cff838d642369f85bb
maialenmunoa <maialenmunoa@gmail.com>
2024-02-11 01:17:05 +0100

    add Link in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 import '../scss/Header.scss';
 import laptopCodeLogo from '../images/laptop-code-solid.svg';
 import adalabLogo from '../images/adalab.png';
@@ -5,11 +7,11 @@ import adalabLogo from '../images/adalab.png';
 function Header() {
     return (
       <header className="header">
-      <a className="header__brand" href="./" title="Haz click para volver a la página inicial">
-        <img className="header__companyLogo" src={laptopCodeLogo} alt="Logo proyectos molones" />
-        <h1 className="header__title">Proyectos molones</h1>
-      </a>
-      <img className="logoSponsor" src={adalabLogo} alt="Logo Adalab" />
+        <Link className="header__brand" to="/" title="Haz click para volver a la página inicial">
+          <img className="header__companyLogo" src={laptopCodeLogo} alt="Logo proyectos molones" />
+          <h1 className="header__title">Proyectos molones</h1>
+        </Link>
+        <img className="logoSponsor" src={adalabLogo} alt="Logo Adalab" />       
       </header>
     )
 }
