69154165643435a595ea98323ced7b0bfbfd01bb
maialenmunoa <maialenmunoa@gmail.com>
2024-02-12 11:14:19 +0100

    add routes and links

--- src/components/pages/LandingPage.jsx
+++ src/components/pages/LandingPage.jsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom";
+
 function LandingPage() {
     return (
         <div>
-            {/* <h1>Landing Page</h1> */}
+            <h1>Landing Page</h1>
+            <p>Welcome to Proyectos Molones!</p>
+            <Link className="button--link" to="/form">
+                Publicar proyecto{" "}
+            </Link>
         </div>
     )
 }
