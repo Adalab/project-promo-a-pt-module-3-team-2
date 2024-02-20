9c53c55e8df7a6bf158d8a4226e03216bf334e40
maialenmunoa <maialenmunoa@gmail.com>
2024-02-20 01:06:10 +0100

    fix form link

--- src/components/pages/ProjectsPage.jsx
+++ src/components/pages/ProjectsPage.jsx
@@ -11,7 +11,7 @@ function ProjectPage() {
         <p className="hero__text">
           Escaparate en línea para recoger ideas a través de la tecnología
         </p>
-        <Link className="button--link--newproyect" to="/detail">
+        <Link className="button--link--newproyect" to="/form">
           NUEVO PROYECTO{" "}
         </Link>
       </section>
